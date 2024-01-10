ecda2afca2d514693077e2d60774ed35e6ed6360
lucas.lima <lucasribeirolima974@gmail.com>
2024-01-10 11:51:30 -0300

    feat: Added frontend inittial files and backend structure

---
+++ backend/functions/graphs.py
@@ -0,0 +1,18 @@
+import plotly.express as px
+
+def showGraphsPlotly(data_frame):
+  # Group by 'Date', calculate the sum of 'Total'
+  total_pages_by_date = data_frame.groupby('Date')['Total'].sum().reset_index()
+
+  # Group by User and Printer, calculate the sum of Total
+  user_printer_totals = data_frame.groupby(['User', 'Printer'])['Total'].sum().reset_index()
+
+  # Group by Printer, calculate the sum of Total
+  printer_totals = data_frame.groupby('Printer')['Total'].sum().reset_index()
+
+  # Create a bar plot with Plotly Express for user and printer totals
+  bar_user_printer_totals = px.bar(user_printer_totals, x='User', y='Total', color='Printer', title='Total Pages Printed by User and Printer')
+  bar_user_printer_totals.show()
+
+  bar_printer_totals = px.bar(printer_totals, x='Printer', y='Total', title='Total Pages Printed by Printer')
+  bar_printer_totals.show()
---
+++ frontend/printerview/src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'PrinterView',
+  description: 'Web Next App to visualizate Papper printer logger data',
+}
+
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
