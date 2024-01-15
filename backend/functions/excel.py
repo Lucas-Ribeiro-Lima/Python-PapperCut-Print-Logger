76e58d525ec491cda439ec62ad6a65bb3c969c97
lucas.lima <lucasribeirolima974@gmail.com>
2024-01-15 15:34:52 -0300

    fix: converting str to a pandas dataframe to excel

--- backend/functions/excel.py
+++ backend/functions/excel.py
@@ -1,11 +1,14 @@
 from io import BytesIO
+import pandas as pd
 from flask import send_file
 from functions import extractCSV
 
 def download_excel(ano, mes):
   #Variavel
   excel_file = BytesIO()
-  data_frame = extractCSV.to_dataframe(ano, mes)
+  json_data = extractCSV.to_dataframe(ano, mes)
+
+  data_frame = pd.DataFrame(json_data)
 
   data_frame.to_excel(excel_file, index=False)
 
