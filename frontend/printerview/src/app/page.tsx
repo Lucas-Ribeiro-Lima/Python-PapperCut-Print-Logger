9457232cafa3c7ddc599beb45bc426212a5c7c37
Lucaslive974 <lucasribeirolima974@gmail.com>
2024-01-14 22:08:35 -0300

    lib: NextUI lib install

--- frontend/printerview/src/app/page.tsx
+++ frontend/printerview/src/app/page.tsx
@@ -1,3 +1,12 @@
+'use client'
+
+import Main from '@/components/ui/Main'
+import { NextUIProvider } from '@nextui-org/react'
+
 export default function Home() {
-  return <main>Printer View</main>
+  return (
+    <NextUIProvider>
+      <Main></Main>
+    </NextUIProvider>
+  )
 }
