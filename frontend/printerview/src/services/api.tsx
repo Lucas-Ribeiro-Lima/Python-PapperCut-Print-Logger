2f3b21afae533571d2a075b2651ec4e0b183fb91
lucas.lima <lucasribeirolima974@gmail.com>
2024-01-17 15:54:54 -0300

    feat: Docker deploy

--- frontend/printerview/src/services/api.tsx
+++ frontend/printerview/src/services/api.tsx
@@ -4,7 +4,7 @@ export function ApiClient() {
   const api = axios.create({
     baseURL: 'http://localhost:5000',
     headers: {
-      Authorization: '9S8kV0mfAXYX618U',
+      Authorization: 'SECRET',
     },
   })
   return api
